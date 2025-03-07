ad8d8ce1c9b3bdc2a4ffc00142749c57edacc787
stilldemaniac <bigsolrac1998@gmail.com>
2025-03-07 13:06:55 -0600

    Create CalculadorDeMasa.py

---
+++ CalculadorDeMasa.py
@@ -0,0 +1,17 @@
+print("Calculadora de masa")
+nombre=input("favor de introducir su nombre: ")
+
+try:
+    peso=float(input("ingrese su peso en Kg con valor mayor a 0 y menor a 180: "))
+except ValueError:
+    print("valor no reconocido, finalizando el programa...")
+
+try:
+    altura=float(input("ingrese su estatura en metros: "))
+except ValueError:
+    print("valor no reconocido, finalizando el programa...")
+
+print("calculando su indice de masa...")
+imc=str((peso/(altura**2)))
+str(imc)
+print(f"{nombre} su indice de masa es de: {imc}")
